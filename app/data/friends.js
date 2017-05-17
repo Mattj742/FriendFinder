9f222313e321dbceab41f438d64d825675a53c9e
Matthew Johnson <mattj742@gmail.com>
2017-05-17 13:30:28 -0400

    completed commit

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var members = [{
+	"name":"Tom Haverford",
+  	"photo":"https://s-media-cache-ak0.pinimg.com/736x/a8/a0/b1/a8a0b1f02925966d3bcd341f9fe929fc.jpg",
+  	"scores":[5, 2, 4, 4, 1, 2, 1, 5, 2, 4]
+}, {
+	"name":"Walter White",
+  	"photo":"https://uproxx.files.wordpress.com/2016/03/walterwhite-uproxx.jpg?quality=100&w=650",
+  	"scores":[4, 5, 1, 2, 5, 5, 3, 5, 5, 5]
+}, {
+	"name":"Erlich Bachman",
+  	"photo":"http://s3.amazonaws.com/assets.piedpiper.pro/app/uploads/2016/06/piedpiper_308_blog_01.jpg",
+  	"scores":[5, 1, 5, 1, 2, 3, 5, 4, 4, 4]
+}, {
+	"name":"Tandy Miller",
+  	"photo":"https://s-media-cache-ak0.pinimg.com/736x/32/47/46/3247465d2b8bae07e5e2e1052a298c08.jpg",
+  	"scores":[1, 1, 4, 1, 5, 1, 5, 3, 1, 1]
+}];
+
+module.exports = members;
